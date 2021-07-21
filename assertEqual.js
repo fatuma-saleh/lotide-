b09ec9cb7966f3a2938b64d81602b365ee572fce
Fatuma <sfatex@gmail.com>
2021-07-21 16:01:27 -0400

    AssertEqual with emojis

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed:" + actual + "===" + expected);
+  } else {
+    console.log("❌❌❌Assertion failed:" + actual + "!==" + expected);
+  }
+};
+
+
+// TEST CODE
+assertEqual("Labs", "Labs");
+assertEqual(1, 5);
