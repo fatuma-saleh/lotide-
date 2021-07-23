a055f552d70b12a1eaeb2e54d844b98dc8879761
Fatuma <sfatex@gmail.com>
2021-07-23 17:07:48 -0400

    initial commit

---
+++ assertArraysEqual.js
@@ -0,0 +1,21 @@
+const eqArrays = function(arr1, arr2) {
+  let flag = false;
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) {
+        flag = true;
+        break;
+      } else {
+        flag = false;
+      }
+    }
+  }
+  // return flag;
+  if (flag === true) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1}===${arr2}`);
+  } else {
+    console.log(`❌❌❌ Assertion failed: ${arr1}!==${arr2}`);
+  }
+
+};
+eqArrays([1, 2, 3], [1, 2, 3]);
