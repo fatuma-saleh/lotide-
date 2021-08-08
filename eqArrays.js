bb4ba9336ea3ed0614c94631fd0033fc80f132a5
Fatuma <sfatex@gmail.com>
2021-08-08 17:46:30 -0400

    added equalArraysTest file

--- eqArrays.js
+++ eqArrays.js
@@ -1,28 +1,14 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual}===${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion failed: ${actual}!==${expected}`);
-  }
-};
-
-const eqArrays = function (arr1, arr2) {
-  let flag = false;
+const eqArrays = function(arr1, arr2) {
   if (arr1.length === arr2.length) {
 
     for (let i = 0; i < arr1.length; i++) {
-
       if (arr1[i] !== arr2[i]) {
-        flag = false;
-        break;
-      } else {
-        flag = true;
+        return false;
       }
     }
+    return true;
   }
-  return flag;
+  return false;
 };
+module.exports = eqArrays;
 
-assertEqual(eqArrays([1, 2,3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), true);
